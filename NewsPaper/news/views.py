2ae87dd86901316469bda0bcd4f0325fc5c2a9a2
Name <email@example.ru>
2022-04-27 23:02:15 +0300

    First Commit
    First Commit

---
+++ NewsPaper/news/views.py
@@ -0,0 +1,124 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+
+
+# Импортируем класс, который говорит нам о том,
+# что в этом представлении мы будем выводить список объектов из БД
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView
+)
+from .models import Post
+from .filters import PostSearch
+from .forms import PostForm
+
+class NewsList(ListView):
+   model = Post
+   ordering = 'title'
+   template_name = 'News.html'
+   context_object_name = 'news'
+   extra_context = {'title': 'Новости'}
+   author = 'author'
+   # queryset = Post.objects.order_by('-dateCreation')
+   paginate_by = 10
+
+   # Переопределяем функцию получения списка товаров
+   def get_queryset(self):
+       # Получаем обычный запрос
+       queryset = super().get_queryset()
+       # Используем наш класс фильтрации.
+       # self.request.GET содержит объект QueryDict, который мы рассматривали
+       # в этом юните ранее.
+       # Сохраняем нашу фильтрацию в объекте класса,
+       # чтобы потом добавить в контекст и использовать в шаблоне.
+       self.filterset = PostSearch(self.request.GET, queryset)
+       # Возвращаем из функции отфильтрованный список товаров
+       return self.filterset.qs
+
+   def get_context_data(self, **kwargs):
+       context = super().get_context_data(**kwargs)
+       # Добавляем в контекст объект фильтрации.
+       context['filterset'] = self.filterset
+       return context
+
+   def post_search(request):
+       f = PostSearch(request.GET,
+                      queryset=Post.objects.all())
+       return render(request,
+                     'news_search.html',
+                     {'filter': f})
+
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'onenews.html'
+    context_object_name = 'onenews'
+
+class PostSearchView(ListView):
+    model = Post  # указываем модель, объекты которой мы будем выводить
+    template_name = 'search.html'  # указываем имя шаблона, в котором будет лежать HTML, в котором будут все инструкции о том, как именно пользователю должны вывестись наши объекты
+    context_object_name = 'NewsSearch'
+    paginate_by = 10  # поставим постраничный вывод в 10 элементов
+    ordering = ['-id']
+    queryset = Post.objects.all()  # Default: Model.objects.all()
+    # form_class = PostForm  # добавляем форм класс, чтобы получать доступ к форме через метод POST
+    # Помощь ментора обычный get_context_data из PostList не подойдет
+    # пагинатор не верно отображает листы
+    def get_filter(self):
+        return PostSearch(self.request.GET, queryset=super().get_queryset())
+
+    def get_queryset(self):
+        return self.get_filter().qs
+
+    def get_context_data(self, **kwargs):
+        return {
+            **super().get_context_data(**kwargs),
+            'filter': self.get_filter(),
+        }
+
+class PostCreateAR(CreateView):
+    # Указываем нашу разработанную форму
+    form_class = PostForm
+    # модель товаров
+    model = Post
+    # и новый шаблон, в котором используется форма.
+    template_name = 'articles_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=True)
+        post.category = "AR"
+        return super().form_valid(form)
+
+class PostCreateNW(CreateView):
+    # Указываем нашу разработанную форму
+    form_class = PostForm
+    # модель товаров
+    model = Post
+    # и новый шаблон, в котором используется форма.
+    template_name = 'news_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=True)
+        post.category = "NW"
+        return super().form_valid(form)
+
+
+
+class PostEditNW(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+
+class PostDeleteNW(DeleteView):
+    model = Post
+    template_name = 'news_delete.html'
+    success_url = reverse_lazy('post_list')
+
+class PostEditAR(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = 'articles_edit.html'
+
+class PostDeleteAR(DeleteView):
+    model = Post
+    template_name = 'articles_delete.html'
+    success_url = reverse_lazy('post_list')
